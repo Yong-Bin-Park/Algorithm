595c518d9c989803d4ae3cebb6027f735de62441
Yong-Bin-Park <dydqls7592@gmail.com>
2022-07-30 23:25:06 +0900

    아직 못품

---
+++ 1차원 배열/OX퀴즈.py
@@ -0,0 +1,21 @@
+#아직 못품
+
+quiz_list = []
+new_list = []
+num = int(input())
+for i in range(num):
+    quiz = input()
+    quiz_list.append(quiz)
+    for j in quiz_list[i]:
+        if j == 'X':
+            
+
+
+    
+    
+#한줄을 통째로 읽어오는데 어떻게 나눌까?
+#pop이용해서 각각 나눈 후 새로운 배열에 넣기
+    
+
+#o가 나오면 순서대로 1, 2, 3~ 더하기
+#x가 나오면 아무것도 안더하고 o의 

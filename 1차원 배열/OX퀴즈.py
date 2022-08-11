caf4e28569b60cf3127065389eaa70f8fdfd2738
Yong-Bin-Park <dydqls7592@gmail.com>
2022-08-11 21:46:17 +0900

    .

--- 1차원 배열/OX퀴즈.py
+++ 1차원 배열/OX퀴즈.py
@@ -1,21 +1,28 @@
-#아직 못품
-
 quiz_list = []
 new_list = []
+result_list = []
+count = 0
+result = 0
 num = int(input())
 for i in range(num):
     quiz = input()
     quiz_list.append(quiz)
-    for j in quiz_list[i]:
-        if j == 'X':
-            
 
-
-    
-    
-#한줄을 통째로 읽어오는데 어떻게 나눌까?
-#pop이용해서 각각 나눈 후 새로운 배열에 넣기
-    
-
-#o가 나오면 순서대로 1, 2, 3~ 더하기
-#x가 나오면 아무것도 안더하고 o의 
+for j in quiz_list:
+    new_list.append(list(j))
+    for k in new_list:
+        for j in k:
+            if j == 'O':
+                count += 1
+            elif j == 'X':
+                for _ in range(1,count+1):
+                    result += _
+                count = 0
+        for _ in range(1,count+1):
+            result += _    
+        result_list.append(result)
+        result = 0
+        count = 0
+                      
+for i in result_list[-num:]:
+    print(i)
